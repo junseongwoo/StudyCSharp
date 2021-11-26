d900c180ccbc136702c2cb3bbe646f6b4c38e783
SolutionA <wnstjd4852@gmail.com>
2021-11-26 18:02:55 +0900

    커밋

--- Part_01/008_ConsoleWriteMulti/Program.cs
+++ Part_01/008_ConsoleWriteMulti/Program.cs
@@ -10,6 +10,11 @@ namespace _008_ConsoleWriteMulti
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("10 이하인 소수 : {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+
+            string primes;
+            primes = String.Format("10 이하인 소수: {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+            Console.WriteLine(primes);
         }
     }
 }
--- Part_01/010_ConsoleFormat/Properties/AssemblyInfo.cs
+++ Part_01/010_ConsoleFormat/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Part_01")]
+[assembly: AssemblyTitle("010_ConsoleFormat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Part_01")]
+[assembly: AssemblyProduct("010_ConsoleFormat")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("6a71e960-786e-4621-beef-79882fabafa1")]
+[assembly: Guid("d3cd315b-a1ab-4630-96ef-62b0c15b1e2b")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
