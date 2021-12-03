659915b2ea9f312be119e91963833268491072ba
SolutionA <wnstjd4852@gmail.com>
2021-12-03 17:07:18 +0900

    반복문 Commit

---
+++ Part_01/049_PrimeNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 소수 판단하기
+ */
+
+namespace _049_PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
